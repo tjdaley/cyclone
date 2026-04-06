f51068b98274af0ae14fc4f668ab16cd1ff61ee1
Tom Daley <tom@powerdaley.com>
2026-04-05 21:13:48 -0500

    fix initial syntax

--- app/db/repositories/attorney.py
+++ app/db/repositories/attorney.py
@@ -5,6 +5,6 @@ from app.db.repositories.base_repo import BaseRepository
 from app.db.models.attorney import AttorneyInDB
 from app.db.supabasemanager import DatabaseManager
 
-class AttorneyRepository(BaseRepository[AttorneyProfileInDB]):
+class AttorneyRepository(BaseRepository[AttorneyInDB]):
     def __init__(self, manager: DatabaseManager):
-        super().__init__(manager, "attorneys", AttorneyProfileInDB)
+        super().__init__(manager, "attorneys", AttorneyInDB)
