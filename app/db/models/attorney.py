5379c7135528408f88f42f94a94d50880f9a7677
Tom Daley <tom@powerdaley.com>
2026-04-05 20:49:32 -0500

    Create attorney data model with Pydantic
    Define data models for attorneys, including FullName and BarAdmission classes.

---
+++ app/db/models/attorney.py
@@ -0,0 +1,30 @@
+"""
+app/db/models/attorney.py - Data model for attorneys
+"""
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import Datetime
+
+class FullName:
+  courtesy_title: Optional[str] = Field(default=None, description="Courtesy title, e.g. Mr., Ms., Dr.")
+  first_name: str = Field(..., description="First name")
+  middle_name: Optional[str] = Field(default=None, description="Middle name or initial. Follow initial with periond")
+  last_name: str = Field(..., description="Last or family name")
+  suffix: Optional[str] = Field(default=None, description="Generational suffix, e.g. Jr., Sr., III, IV")
+
+class BarAdmission:
+  bar_number: str = Field(..., description="Bar card number")
+  state: str = Field(..., description="State of Admission")
+
+class Attorney:
+  name: FullName
+  office_id: int = Field(..., description="Index into offices table")
+  email: str
+  telephone: str
+  slug: str = Field(..., description="Second-level domain and alternate attorney identifier. Unique key")
+  bar_admissions = List[BarAdmission] = Field(..., description="Bar admissions for this attorneyh")
+
+class AttorneyInDB(Attorney):
+  id: int = Field(..., description="Unique key for this table")
+  created_at: Datetime = Field(..., description="Date/time this record was created. Set by database")
+  updated_at: Optional[Datetime] = Field(default=None, description="Date/time this record was last updated. Set by database")
